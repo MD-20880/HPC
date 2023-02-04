8f5f73f1e1e2f60b47ec5579e9840b2e37e4e772
Mingzhang Deng <wu20880@bristol.ac.uk>
2023-02-04 16:55:56 +0000

    sh script

---
+++ tb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e # exit on error
+
+
+FILEPATH=`pwd` 
+
+echo "Compiling the code"
+make # compile the code
+
+#Collect Environment Variables
+
+
+
+echo "Running"
+sbatch -W job_submit_d2q9-bgk #Submit the Job
+echo "Job complete"
+
+
+echo "Checking"
+make check #Check the Result
+echo "Check complete"
+
+#Archive the results
+echo "Archiving"
+FILENAME='ls | wc -l | awk "{printf( \"%05d\n\" , $1)}'
+mkdir -r $FILEPATH/Archive/$FILENAME
+for i in av_vels.dat final_state.dat d2q9-bgk.out d2q9-bgk; do
+    mv $i $FILEPATH/Archive/$FILENAME
+done
+for i in d2q9-bgk.c Makefile; do
+    cp $i $FILEPATH/Archive/$FILENAME
+done
+
+
+
+
+
+
+

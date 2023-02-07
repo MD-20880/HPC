8eb3be70f77d580d58ed69d88a9a56372c7f18b4
Mingzhang Deng <wu20880@bristol.ac.uk>
2023-02-07 13:19:29 +0000

    sync

--- Makefile
+++ Makefile
@@ -13,6 +13,10 @@ REF_AV_VELS_FILE=check/128x128.av_vels.dat
 
 all: $(EXE)
 
+
+prof:
+	$(CC) $(CFLAGS) -pg $(EXE).c $(LIBS) -o $(EXE)
+
 $(EXE): $(EXE).c
 	$(CC) $(CFLAGS) $^ $(LIBS) -o $@
 
--- tb.sh
+++ tb.sh
@@ -12,7 +12,18 @@ make # compile the code
 
 
 
+
+
 echo "Running"
+if [ $(OSTYPE) == darwin22.0]; then
+    echo "Running on Mac"
+    ./d2q9-bgk input_128x128.params obstacles_128x128.dat
+else
+    echo "Running on BC4"
+    sbatch -W job_submit_d2q9-bgk #Submit the Job
+
+
+fi
 # sbatch -W job_submit_d2q9-bgk #Submit the Job
 echo "Job complete"
 
@@ -23,11 +34,19 @@ echo "Check complete"
 
 #Archive the results
 echo "Archiving"
-ls | wc -l | awk '{printf( "%05d\n" , $1)}' | FILENAME=
+FILENAME=$(ls ./Archive | wc -l | awk '{printf( "%05d\n" , $1)}')
+echo $FILENAME
+
+if [FILENAME == ""]; then
+    echo "Generate Filename Failed"
+    exit 1
+fi 
+
 mkdir -p $FILEPATH/Archive/$FILENAME
 for i in av_vels.dat final_state.dat d2q9-bgk.out d2q9-bgk; do
     mv $i $FILEPATH/Archive/$FILENAME
 done
+
 for i in d2q9-bgk.c Makefile; do
     cp $i $FILEPATH/Archive/$FILENAME
 done
